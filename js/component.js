a44f4e9765d8d74d5e490b7101e257058b9677d6
sahil <sahil00askned@gmail.com>
2020-10-04 16:57:13 +0530

    extra changes

--- js/component.js
+++ js/component.js
@@ -20,21 +20,34 @@ const foot=Vue.component('foot',{
    template:' <footer ><div class="endFooter"><span>© Copyright 2020 by Armin Ronacher.</span><span> Content licensed under the Creative Commons attribution-noncommercial-sharealike License.</span><span> Contact me via mail, twitter, github or bitbucket.</span><span> More info: imprint. Subscribe to Atom feed (or RSS)</span></div></footer>   '
 });
 Vue.component('currency_con',{
-   template:'<div class="l_cont"><h3>Currency Convertor</h3><div class="data_container"><input v-model="dollars" type="text" placeholder="$"></input></div><p>In Rs:{{rupe}} </p></div> ',
+   template:'<div><div class="l_cont"><h3>Currency Convertor</h3><div class="main_cont"><div class="box1 abc"><p><strong> Enter Dollars</strong></p><p>  <input type="text" v-model="dollars"/></p> <p><input type="text" v-if="d_rs>0" v-model="d_rs" readonly/></p></div><div class="box2 abc"> <p><strong> Enter chinese</strong></p><p> <input  v-model="chineseyan" type="text"/></p> <p><input v-if="c_rs>0" v-model="c_rs" type="text" readonly/></p></div><div class="box3 abc"><p><strong>Enter Japanas</strong></p><p><input type="text" v-model="Japanas" placeholder="euro"/></p> <p><input v-if="j_rs>0" v-model="j_rs" type="text" readonly/></p></div><div class="box4 abc"><p><button v-on:click="addition" class="btn btn-alert">Bill</button></p> </div><div class="box5 abc"><p><input v-if="total>0" v-model="total"  type="text" readonly/></p> </div></div></div></div> ',
    data(){
-      return{dollars:0,rupee:0}
+      return{
+         dollars:0,
+         Japanas:0,
+         chineseyan:0,
+         total:0
+                  }
    },
    computed:{
-      rupe:{
-      get :function(){
+     d_rs:function(){
         return parseInt(this.dollars)*75
       },
-      set:function(dvalue){
-         this.rupee=int(dvalue)*75
-         console.log(this.rupee)
+      j_rs:function(){
+         return parseInt(this.Japanas)*0.70
+      },
+      c_rs:function(){
+         return parseInt(this.chineseyan)*10.80
       }
 
-   }}
+   
+   },
+   methods:{
+      addition:function(){
+      this.total=this.d_rs+this.j_rs+this.c_rs
+      console.log(this.total)
+      }
+   }
 });
 
 var vm = new Vue({
