f0e6088e2794e0dd7e0f511b68e6475b252f1c6b
sahil <sahil00askned@gmail.com>
2020-10-04 18:56:24 +0530

    sahil_work

--- js/component.js
+++ js/component.js
@@ -3,7 +3,7 @@ var maker=Vue.component('maker',{
     template : '<section class="dashboard_area "><div class=l_cont ><div class="data_container "><div class="date_box ">3/2020/21</div><div class="detail_contianer ml-5"><strong class="link_headings"></strong><a href="view/">Named Routes</a><strong>&mdash;</strong>aSometimes it is more convenient to identify a route with a name, especially when linking to a route or performing navigations.</div> </div><div class=l_cont ><div class="data_container "><div class="date_box ">3/2020/21</div><div class="detail_contianer ml-5"><strong class="link_headings"></strong><a href="view/">Named Routes</a><strong>&mdash;</strong>aSometimes it is more convenient to identify a route with a name, especially when linking to a route or performing navigations.</div> </div><div class=l_cont ><div class="data_container "><div class="date_box ">3/2020/21</div><div class="detail_contianer ml-5"><strong class="link_headings"></strong><a href="view/">Named Routes</a><strong>&mdash;</strong>aSometimes it is more convenient to identify a route with a name, especially when linking to a route or performing navigations.</div> </div><div class=l_cont ><div class="data_container "><div class="date_box ">3/2020/21</div><div class="detail_contianer ml-5"><strong class="link_headings"></strong><a href="view/">Named Routes</a><strong>&mdash;</strong>aSometimes it is more convenient to identify a route with a name, especially when linking to a route or performing navigations.</div> </div><div class=l_cont ><div class="data_container "><div class="date_box ">3/2020/21</div><div class="detail_contianer ml-5"><strong class="link_headings"></strong><a href="view/">Named Routes</a><strong>&mdash;</strong>aSometimes it is more convenient to identify a route with a name, especially when linking to a route or performing navigations.</div> </div></section>'
  });
  Vue.component('heade',{
-   template:'<div class="header" id="app"> <div class="abt"><a href="#">Sahil abouts</a><span>Blogs and writings</span></div><div class="link"><ul class="nv-links"><li><router-link to=/>blog</router-link></li><li><router-link to=currency_conv>Currency app</router-link></li><li>tags</li><li><button class="btn btn-primary" v-on:click="on_off"><span v-if="show">on </span><span v-else> off  </span>Button</button></li></ul></div>  </div>',
+   template:'<div class="header" id="app"> <div class="abt"><a href="#">Sahil abouts</a><span>Blogs and writings</span></div><div class="link"><ul class="nv-links"><li><router-link to=/>blog</router-link></li><li><router-link to=form>Form</router-link></li><li><router-link to=currency_conv>Currency app</router-link></li><li>tags</li><li><button class="btn btn-primary" v-on:click="on_off"><span v-if="show">on </span><span v-else> off  </span>Button</button></li></ul></div>  </div>',
    data(){
       return{show:true}
    },
@@ -49,6 +49,69 @@ const currency_con=Vue.component('currency_con',{
       }
    }
 });
+var form=Vue.component('form',{
+   template:' <div class=" container " id="app"> <div class="row"> <div class="col-12 mx-auto "> <div class=" mx-auto d-flex mt-4 flex-column justify-content-center align-items-center form-group "> <h1>Add New Data</h1> </div> <div class=" mx-auto d-flex mt-4 flex-column justify-content-center align-items-center form-group "> <input v-bind:style="{borderColor:form.error_list.name_class}" class="col-6 form-control" type="text" v-model="form.name" placeholder="enter name" /> </div> <div class=" mx-auto d-flex flex-column justify-content-center align-items-center form-group "> <input v-bind:style="{borderColor:form.error_list.email_class}" class="col-6 form-control" type="text" v-model="form.email" placeholder="enter Email" /> </div> <div class=" mx-auto d-flex flex-column justify-content-center align-items-center form-group "> <input v-bind:style="{borderColor:form.error_list.contact_class}" class="col-6 form-control" type="text" v-model="form.contact" placeholder="enter contact" /> </div> <div class=" mx-auto d-flex flex-column justify-content-center align-items-center form-group "> <input v-bind:style="{borderColor:form.error_list.department_class}" class="col-6 form-control" type="text" v-model="form.department" placeholder="enter Department" /> <button class="btn btn-primary mt-8" v-on:click="save_data">Submit</button> </div> </div> </div>',
+   data(){
+      return {form:{
+          name:"",
+          email:"",
+          contact:"",
+          department:"",
+          have_error:0,
+          error_list:{
+              name_class:"",
+              email_class:"",
+              contact_class:"",
+              department_class:""
+          }
+      }
+   }
+  },methods:{
+      save_data:function(){
+          if(this.form.name==""){
+              this.form.have_error+=1
+              this.form.error_list.name_class="red"
+          }
+          else{
+           
+              this.form.error_list.name_class=""
+        
+          }
+          if(this.form.email==""){
+             this.form.have_error+=1
+            this.form.error_list.email_class="red"
+          }
+          else{
+              this.form.error_list.email_class=""
+   
+          }
+          if(this.form.contact==""){
+              this.form.have_error+=1
+              this.form.error_list.contact_class="red"
+          }
+          else{
+              
+              this.form.error_list.contact_class=""
+        
+          }
+           if(this.form.department==""){
+              this.form.have_error+=1
+              this.form.error_list.department_class="red"
+          }
+          else{
+          
+             
+              this.form.error_list.department_class=""
+        
+          }
+          console.log("number of errors",this.form.have_error)
+          this.form.have_error=0
+        
+        
+
+      }
+  },
+})
 var routes=[
    {
       path:"/currency_conv",
@@ -59,8 +122,12 @@ var routes=[
       component:maker
    },
    {
-      path:"dashboard",
+      path:"/dashboard",
       component:maker
+   },
+   {
+      path:"/form",
+      component:form,
    }
 ]
 
